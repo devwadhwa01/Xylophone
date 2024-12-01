4078ed1eb6b70914fe5737f81720dd8dfc54ac0e
Dev Wadhwa <devwadhwa01@gmail.com>
2024-12-02 02:10:36 +0530

    Added icons

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,46 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  TextButton keys(String noteSound, Color color, String noteName){
-    double buttonHeight = 70.0;
-    return TextButton(
-      style: const ButtonStyle(enableFeedback: false, overlayColor: WidgetStateColor.transparent, shadowColor: WidgetStateColor.transparent, splashFactory: NoSplash.splashFactory),
+
+  ElevatedButton keys(String noteSound, Color? color, String noteName) {
+    double buttonHeight = 75.0;
+    return ElevatedButton(
+      style: ButtonStyle(
+          enableFeedback: false,
+          backgroundColor:  WidgetStateProperty.all<Color?>(color),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+          overlayColor: WidgetStateColor.transparent,
+          shadowColor: WidgetStateColor.transparent,
+          splashFactory: NoSplash.splashFactory),
       onPressed: () {
         playMusic(noteSound);
       },
-      child: Container(
-        color: color,
+      child: SizedBox(
         width: double.infinity,
         height: buttonHeight,
-        child: Center(child: Text(noteName, style: const TextStyle(fontSize: 30.0, color: Colors.white, fontWeight: FontWeight.bold),)),
+        child: Center(
+          child: Text(
+            noteName,
+            style: const TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Avocado",
+            ),
+          ),
+        ),
       ),
     );
   }
 
+  final int colorContrast = 400;
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -34,25 +55,32 @@ class MyApp extends StatelessWidget {
           title: const Center(
             child: Text(
               'Xylophone',
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontFamily: "Cinzel",
+                  fontWeight: FontWeight.bold),
             ),
           ),
           backgroundColor: Colors.black26,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            keys("C2", Colors.red,"C"),
-            keys("B", Colors.orange,"B"),
-            keys("A", Colors.yellow,"A"),
-            keys("G", Colors.green,"G"),
-            keys("F", Colors.blue,"F"),
-            keys("E1", Colors.indigo,"E"),
-            keys("D1", Colors.deepPurple,"D"),
-            keys("C", Colors.purple,"C")
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              keys("C2", Colors.red[colorContrast], "C"),
+              keys("B", Colors.orange[colorContrast], "B"),
+              keys("A", Colors.yellow[colorContrast], "A"),
+              keys("G", Colors.green[colorContrast], "G"),
+              keys("F", Colors.blue[colorContrast], "F"),
+              keys("E1", Colors.indigo[colorContrast], "E"),
+              keys("D1", Colors.deepPurple[colorContrast], "D"),
+              keys("C", Colors.purple[colorContrast], "C")
+            ],
+          ),
         ),
-        backgroundColor: Colors.white10,
+        backgroundColor: Colors.white24,
       ),
     );
   }
